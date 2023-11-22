d643e4a848cade1f543aa40666e15544cf5d3c94
Thean Cheat Lim <thean-cheat.lim@keysight.com>
2023-11-22 14:45:49 +0800

    Replay BGM Audio Files + Shuffle

--- src/BackgroundMusic.js
+++ src/BackgroundMusic.js
@@ -1,85 +1,99 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback, useRef } from 'react';
 
 function BackgroundMusic({ folderId }) {
-  const [audio, setAudio] = useState(new Audio());
-  const [audioFiles, setAudioFiles] = useState(() => {
-    const cachedAudioFiles = localStorage.getItem('cachedAudioFiles');
-    return cachedAudioFiles ? JSON.parse(cachedAudioFiles) : [];
-  });
+  const audioObjects = useRef({});
+  const [audioFiles, setAudioFiles] = useState([]);
+  const [remainingAudioIndices, setRemainingAudioIndices] = useState([]);
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [hasFetched, setHasFetched] = useState(false);
 
-  useEffect(() => {
-    const playOnInteraction = () => {
-      if (audioFiles.length > 0) {
-        const randomIndex = Math.floor(Math.random() * audioFiles.length);
-        const randomAudioUrl = audioFiles[randomIndex].webContentLink;
-        setAudio(new Audio(randomAudioUrl));
-        audio.loop = true;
-        audio.volume = 0.5;
-        audio.play();
-        document.removeEventListener('mousedown', playOnInteraction);
-        document.removeEventListener('touchstart', playOnInteraction);
+  const playAudio = useCallback(() => {
+    if (!isAudioPlaying && remainingAudioIndices.length > 0) {
+      setIsAudioPlaying(true);
+      const randomIndex = remainingAudioIndices[Math.floor(Math.random() * remainingAudioIndices.length)];
+      const randomAudioUrl = audioFiles[randomIndex].webContentLink;
+      let newAudio = audioObjects.current[randomAudioUrl];
+      if (!newAudio) {
+        newAudio = new Audio(randomAudioUrl);
+        audioObjects.current[randomAudioUrl] = newAudio;
       }
-    };
-
-    document.addEventListener('mousedown', playOnInteraction);
-    document.addEventListener('touchstart', playOnInteraction);
-
-    return () => {
-      document.removeEventListener('mousedown', playOnInteraction);
-      document.removeEventListener('touchstart', playOnInteraction);
-    };
-    // eslint-disable-next-line
-  }, [audioFiles]);
-
-  useEffect(() => {
-    const playAudio = () => {
-      if (audio.paused) {
-        try {
-          audio.play();
-        } catch (error) {
-          console.error('Error playing audio:', error);
-        }
-      }
-    };
-
-    window.addEventListener('click', playAudio);
-
-    return () => {
-      window.removeEventListener('click', playAudio);
-    };
-  }, [audio]);
-
-  useEffect(() => {
-    if (audioFiles.length === 0) {
-      fetchAudioFiles();
+      newAudio.loop = false;
+      newAudio.volume = 0.5;
+      newAudio.play().then(() => {
+        const updatedIndices = remainingAudioIndices.filter((index) => index !== randomIndex);
+        setRemainingAudioIndices(updatedIndices);
+      }).catch((error) => console.error('Error playing audio: ', error));
+    } else if (!isAudioPlaying && remainingAudioIndices.length === 0 && audioFiles.length > 0) {
+      // All songs have been played, reset the playlist
+      const shuffledIndices = audioFiles.map((_, index) => index);
+      setRemainingAudioIndices(shuffledIndices);
     }
-    // eslint-disable-next-line
-  }, [folderId, audioFiles.length]);
+  }, [isAudioPlaying, remainingAudioIndices, audioFiles]);
+
+  const fetchAudioFiles = useCallback(() => {
+    if (sessionStorage.getItem('cachedAudioFiles')) {
+      const cachedAudios = JSON.parse(sessionStorage.getItem('cachedAudioFiles'));
+      setAudioFiles(cachedAudios);
+      const shuffledIndices = cachedAudios.map((_, index) => index);
+      setRemainingAudioIndices(shuffledIndices);
+    } else {
+      fetch(`https://www.googleapis.com/drive/v3/files?q='${folderId}' in parents&fields=files(id,webContentLink,name,mimeType)&key=AIzaSyDMl9CkM-8_2MFm5I239oYbCuxRqOjXVoc`)
+        .then((response) => response.json())
+        .then((data) => {
+          const filteredAudios = data.files.filter((file) => file.mimeType.startsWith('audio/'));
+          setAudioFiles(filteredAudios);
+          const shuffledIndices = filteredAudios.map((_, index) => index);
+          setRemainingAudioIndices(shuffledIndices);
+          sessionStorage.setItem('cachedAudioFiles', JSON.stringify(filteredAudios));
+        })
+        .catch((error) => console.error('Error fetching audio files: ', error));
+    }
+  }, [folderId]);
+  
+  useEffect(() => {
+    if (!hasFetched) {
+      if (sessionStorage.getItem('cachedAudioFiles')) {
+        const cachedAudios = JSON.parse(sessionStorage.getItem('cachedAudioFiles'));
+        setAudioFiles(cachedAudios);
+        const shuffledIndices = cachedAudios.map((_, index) => index);
+        setRemainingAudioIndices(shuffledIndices);
+      } else {
+        fetchAudioFiles();
+      }
+      setHasFetched(true);
+    }
+  }, [folderId, hasFetched, fetchAudioFiles]);
 
   useEffect(() => {
-    audio.addEventListener('ended', () => {
-      audio.currentTime = 0; // Reset the audio to the beginning
-      audio.play(); // Replay the audio when it ends
-    });
+    document.addEventListener('mousedown', playAudio);
 
     return () => {
-      audio.removeEventListener('ended', () => {
-        audio.currentTime = 0;
-        audio.play();
-      });
+      document.removeEventListener('mousedown', playAudio);
     };
-  }, [audio]);
+  }, [playAudio]);
 
-  const fetchAudioFiles = () => {
-    fetch(`https://www.googleapis.com/drive/v3/files?q='${folderId}' in parents&fields=files(id,webContentLink,name,mimeType)&key=AIzaSyDMl9CkM-8_2MFm5I239oYbCuxRqOjXVoc`)
-      .then((response) => response.json())
-      .then((data) => {
-        const filteredAudios = data.files.filter((file) => file.mimeType.startsWith('audio/'));
-        setAudioFiles(filteredAudios);
-        localStorage.setItem('cachedAudioFiles', JSON.stringify(filteredAudios));
-      })
-      .catch((error) => console.error('Error fetching audio files: ', error));
-  };
+  useEffect(() => {
+    const handleAudioEnd = () => {
+      setIsAudioPlaying(false);
+      playAudio();
+    };
+  
+    const currentAudioObjects = audioObjects.current;
+  
+    if (currentAudioObjects) {
+      Object.values(currentAudioObjects).forEach((audio) => {
+        audio.addEventListener('ended', handleAudioEnd);
+      });
+    }
+  
+    return () => {
+      if (currentAudioObjects) {
+        Object.values(currentAudioObjects).forEach((audio) => {
+          audio.removeEventListener('ended', handleAudioEnd);
+        });
+      }
+    };
+  }, [audioObjects, playAudio]);  
 
   return null; // No visible UI elements for BackgroundMusic
 }
